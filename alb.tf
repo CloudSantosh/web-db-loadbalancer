373a5972b1f0b977101e5966dbe4bf82d33434ff
Santosh <126384863+CloudSantosh@users.noreply.github.com>
2023-05-12 11:23:51 +0300

    uploading from local computer

---
+++ alb.tf
@@ -0,0 +1,44 @@
+
+#Create an ALB target group
+resource "aws_lb_target_group" "alb-TG" {
+  name     = "alb-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+}
+
+#Create Load balancer
+resource "aws_lb" "my-aws-alb" {
+  name               = "my-aws-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.my_vpc_sg.id]
+  subnets            = [aws_subnet.public_sub2a.id, aws_subnet.public_sub2b.id]
+}
+
+# Create Load balancer listner rule
+resource "aws_lb_listener" "lb_lst" {
+  load_balancer_arn = aws_lb.my-aws-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb-TG.arn
+  }
+}
+
+#Load balancer-Target group attachment
+resource "aws_lb_target_group_attachment" "my-aws-alb" {
+  target_group_arn = aws_lb_target_group.alb-TG.arn
+  target_id        = aws_instance.Pub2a_ec2.id
+  port             = 80
+}
+
+#Load balancer-Target group attachment
+resource "aws_lb_target_group_attachment" "my-aws-alb2" {
+  target_group_arn = aws_lb_target_group.alb-TG.arn
+  target_id        = aws_instance.Pub2b_ec2.id
+  port             = 80
+}
+
